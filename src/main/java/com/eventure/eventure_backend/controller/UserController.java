46cd98efe0e550655cd2995ff986ceeb42e1f9a1
Keron Ranchhodbhai Balar <balarkeron11@gmail.com>
2025-07-02 13:12:59 -0400

    Add .gitignore

---
+++ src/main/java/com/eventure/eventure_backend/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.eventure.eventure_backend.controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/eventure/eventure_backend/service/UserService.java
@@ -0,0 +1,4 @@
+package com.eventure.eventure_backend.service;
+
+public class UserService {
+}
